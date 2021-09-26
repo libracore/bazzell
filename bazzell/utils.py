f4cf4ec5b9f264f68d853e7b946fd49bd0d5e4d9
lasalesi <larsericmueller@gmail.com>
2021-09-26 20:21:38 +0200

    Update utils.py
    hotfix

--- bazzell/utils.py
+++ bazzell/utils.py
@@ -13,7 +13,7 @@ def set_item_description_equals_to_stock_uom():
     woocommerce_items = frappe.db.sql("""SELECT `item_code`, `stock_uom` FROM `tabItem` WHERE `item_group` = 'WooCommerceItem' AND `has_variants` = 0""", as_dict=True)
     for woocommerce_item in woocommerce_items:
         frappe.db.sql("""UPDATE `tabItem` SET `description` = '{new_description}' WHERE `item_code` = '{item_code}'""".format(
-            new_description=woocommerce_item.stock_uom, item_code=wooc$
+            new_description=woocommerce_item.stock_uom, item_code=woocommerce_item.item_code), as_list=True)
 
 @frappe.whitelist()
 def get_stock_items(warehouse):
