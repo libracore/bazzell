6d7b07891efb4ba435c62b232e0a3b7d031949cd
Joel <joel.mesmer@libracore.com>
2025-08-29 16:33:18 +0200

    #4

--- bazzell/bazzell/stocktransfer.py
+++ bazzell/bazzell/stocktransfer.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2020-2024, libracore.com and Contributors
+# Copyright (c) 2020-2025, libracore.com and Contributors
 # MIT License. See license.txt
 
 from __future__ import unicode_literals
@@ -34,95 +34,107 @@ def repack_to_single_uom(debug=False):
         # loop templates, check for repacking
         for template in templates_in_stock:
             repack_template(template['variant_of'], warehouse_for_repacks, stock_difference_account, debug)
-            
+        
     return
 
 def repack_template(template, warehouse_for_repacks, stock_difference_account, debug=False):
-    # per template: repack?
-    query_variant_stock_levels = """SELECT 
+    query_variant_stock_levels = """
+        SELECT 
             `tabItem`.`item_code`,
-            `tabItem`.`item_name`,
-            `tabItem`.`variant_of`,
-            `tabItem`.`stock_uom`,
             `tabItem`.`safety_stock`,
             IFNULL(`tabBin`.`actual_qty`, 0) AS `actual_qty`,
-            `tabUOM`.`value` AS `factor`,
-            IFNULL(`tabBin`.`actual_qty`, 0) * `tabUOM`.`value` AS `actual_pcs`
+            `tabUOM`.`value` AS `uom_value`
         FROM `tabItem` 
         LEFT JOIN `tabBin` ON `tabBin`.`item_code` = `tabItem`.`item_code` AND `tabBin`.`warehouse` = '{warehouse}'
-        JOIN `tabUOM` ON `tabUOM`.`name` = `tabItem`.`stock_uom`
+        LEFT JOIN `tabUOM` ON `tabUOM`.`name` = `tabItem`.`stock_uom`
         WHERE `tabItem`.`variant_of` = '{variant_of}'
-        ORDER BY `factor` ASC;""".format(variant_of=template, warehouse=warehouse_for_repacks)
+        AND `tabUOM`.`value` > 0
+        AND `tabItem`.`disabled` != 1
+        ORDER BY `uom_value` ASC;""".format(variant_of=template, warehouse=warehouse_for_repacks)
    
     variants_in_stock = frappe.db.sql(query_variant_stock_levels, as_dict=True)
+
+    if len(variants_in_stock) < 2:
+        if debug:
+            print("Kein Umlagerungsziel gefunden")
+        return
     
-    #print("variants {0}".format(variants_in_stock))
-    single_item_details = variants_in_stock[0]
-    # umlagern erforderlich?
-    if (single_item_details['actual_qty'] < single_item_details['safety_stock']) or (single_item_details['actual_qty'] < 1):
-        # umlagern möglich?
-        if (len(variants_in_stock) > 1):
-            # has other variants, check if any of them have stock
-            source_item_details = {'item_code': None, 'actual_pcs': 0, 'factor': 0}
-            for variant in variants_in_stock:
-                if variant['item_code'] != single_item_details['item_code']:
-                    if variant['actual_pcs'] >= source_item_details['actual_pcs']:
-                        source_item_details = {'item_code': variant['item_code'], 'actual_pcs': variant['actual_pcs'], 'factor': variant['factor']}
-            if source_item_details['actual_pcs'] > 0:
-                # umlagern
-                
-                # quelle ausbuchen
-                if debug:
-                    print("{1}::{0} 1x ausbuchen".format(source_item_details['item_code'], template))
-                #create material_issue of item with highest amount in stock
-                new_mi = frappe.get_doc({
-                    "doctype": "Stock Entry",
-                    "stock_entry_type": "Material Issue",
-                    "from_warehouse": warehouse_for_repacks,
-                    "items": [
-                        {
-                            "item_code": source_item_details['item_code'],
-                            "qty": 1,
-                            "expense_account": stock_difference_account
-                        }
-                    ],
-                })
-                new_record = new_mi.insert()
-                if debug:
-                    print("{0}".format(new_record.name))
-                target_valuation_rate = (new_record.items[0].valuation_rate / source_item_details['factor'])
-                if debug:
-                    print("Target valuation: {0}".format(target_valuation_rate))
-                if not debug:
-                    new_mi.submit()
-                # create material receipt of single product that needs more stock
-                if debug:
-                   print("{2}::{0} {1}x einbuchen".format(single_item_details['item_code'], source_item_details['factor'], template))
-                new_mr = frappe.get_doc({
-                    "doctype": "Stock Entry",
-                    "stock_entry_type": "Material Receipt",
-                    "to_warehouse": warehouse_for_repacks,
-                    "items": [
-                        {
-                            "item_code": single_item_details['item_code'],
-                            "qty": source_item_details['factor'],
-                            "basic_rate": target_valuation_rate,
-                            "expense_account": stock_difference_account
-                        }
-                    ],
-                })
-                new_mr.insert()
-                if debug:
-                    print("{0}".format(new_mr.name))
-                if not debug:
-                    new_mr.submit()
-            else:
-                if debug:
-                    print("{1}::{0}: kein grösseres Gebinde gefunden oder dieses hat keinen Lagerbestand".format(single_item_details['item_code'], template))
+    min_uom_item = variants_in_stock[0]
+    max_uom_item = variants_in_stock[-1]
+
+    # Prüfung dass beide UOMs > 0 sind
+    # if (float(min_uom_item['uom_value']) + float(max_uom_item['uom_value'])) <= 1:
+    #     if debug:
+    #         print("Verarbeitung wird abgebrochen, mind. eine UOM ist 0")
+    #     return
+
+    # Entspricht die uom_max einem Vielfachen von uom_min?
+    if max_uom_item['uom_value'] % min_uom_item['uom_value'] != 0:
+        if debug:
+            print("Abbruch, grössere UOM entspricht nicht einem Vielfachen der kleineren UOM")
+        return
+
+    # Umlagerung erforderlich?
+    if (min_uom_item['actual_qty'] < min_uom_item['safety_stock']) or (min_uom_item['actual_qty'] < 1):
+
+        # Besitz die grössere UOM Lager?
+        if max_uom_item['actual_qty'] < 1:
+            if debug:
+                print("{1}::{0}: kein grösseres Gebinde gefunden oder dieses hat keinen Lagerbestand".format(min_uom_item['item_code'], template))
+            return
+        
+        # Quelle ausbuchen
+        if debug:
+            print("{1}::{0} 1x ausbuchen".format(max_uom_item['item_code'], template))
+
+        #create material_issue of item with highest amount in stock
+        mi = frappe.get_doc({
+            "doctype": "Stock Entry",
+            "stock_entry_type": "Material Issue",
+            "from_warehouse": warehouse_for_repacks,
+            "items": [
+                {
+                    "item_code": max_uom_item['item_code'],
+                    "qty": 1,
+                    "expense_account": stock_difference_account
+                }
+            ],
+        }).insert()
+
+        if debug:
+            print("{0}".format(mi.name))
+        else:
+            mi.submit()
+
+        target_valuation_rate = (mi.items[0].valuation_rate / max_uom_item['uom_value']) * min_uom_item['uom_value']
+        target_qty = int(max_uom_item['uom_value'] / min_uom_item['uom_value'])
+
+        if debug:
+            print("{2}::{0} {1}x einbuchen".format(min_uom_item['item_code'], target_qty, template))
+            print("Target valuation: {0}".format(target_valuation_rate))
+            print("Target QTY: {0}".format(target_qty))
+        
+        mr = frappe.get_doc({
+            "doctype": "Stock Entry",
+            "stock_entry_type": "Material Receipt",
+            "to_warehouse": warehouse_for_repacks,
+            "items": [
+                {
+                    "item_code": min_uom_item['item_code'],
+                    "qty": target_qty,
+                    "basic_rate": target_valuation_rate,
+                    "expense_account": stock_difference_account
+                }
+            ],
+        }).insert()
+
+        if debug:
+            print("{0}".format(mr.name))
+        else:
+            mr.submit()
     else:
         if debug:
-            print("{1}::{0}: nichts zum umlagern".format(single_item_details['item_code'], template))
-    
+            print("{1}::{0}: nichts zum umlagern".format(min_uom_item['item_code'], template))
     return
 
 @frappe.whitelist()
